7eb0ff4abaeb85287206c90405aa3810424f8332
cyberx <cyberx@cyberx-K93SM>
2017-12-23 16:55:42 +0100

    configurazione

--- todo/src/main/java/com/cyberx/todo/dao/TodoDaoImpl.java
+++ todo/src/main/java/com/cyberx/todo/dao/TodoDaoImpl.java
@@ -2,22 +2,39 @@ package com.cyberx.todo.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
 import com.cyberx.todo.model.Todo;
+
 @Repository
 public class TodoDaoImpl implements TodoDao {
+
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public SessionFactory getSessionFactory() {
+		return this.sessionFactory;
+	}
+	
+	private Session getSession() {
+		return this.getSessionFactory().getCurrentSession();
+	}
 
 	@Override
 	public void add(Todo todo) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Todo todo) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -35,7 +52,7 @@ public class TodoDaoImpl implements TodoDao {
 	@Override
 	public void delete(int id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- todo/src/main/java/com/cyberx/todo/service/TodoServiceImpl.java
+++ todo/src/main/java/com/cyberx/todo/service/TodoServiceImpl.java
@@ -2,37 +2,49 @@ package com.cyberx.todo.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cyberx.todo.dao.TodoDao;
 import com.cyberx.todo.vo.TodoVo;
 
 @Service
 public class TodoServiceImpl implements TodoService {
 
+	@Autowired(required=true)
+	private TodoDao todoDao;
+	
+	@Transactional
+	public void setTodoDao(TodoDao todoDao) {
+		this.todoDao = todoDao;
+	}
+	@Transactional
 	@Override
 	public void add(TodoVo todo) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@Transactional
 	@Override
 	public void update(TodoVo todo) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@Transactional
 	@Override
 	public void remove(int id) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	@Transactional
 	@Override
 	public List<TodoVo> list() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Transactional
 	@Override
 	public List<TodoVo> search(String key) {
 		// TODO Auto-generated method stub
